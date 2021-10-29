9f2cdcff50c99a85ca9998d62647d2da116a9fe3
Sarah Kastama <sarahkastama@gmail.com>
2021-10-29 13:36:59 -0700

    12-13

--- easy(7-25).py
+++ easy(7-25).py
@@ -1,3 +1,4 @@
+# Number 7
 # # This is the class of the input root. Do not edit it.
 # class BinaryTree:
 #     def __init__(self, value):
@@ -21,20 +22,20 @@
 #     branchSumsCall(root.left, runningSum, sums)
 # 	branchSumsCall(root.right, runningSum, sums)
 
+# Number 8
 # def nodeDepths(node, depth = 0):
 #     if (node == None):
 # 	    return 0
 #     return depth + nodeDepths(node.left, depth+1) + nodeDepths(node.right, depth+1)
 
 
-
+# Number 9
 # # This is the class of the input binary tree.
 # class BinaryTree:
 #     def __init__(self, value):
 #         self.value = value
 #         self.left = None
 #         self.right = None
-
 
 # # for the depthFirstSearch method.
 # # Feel free to add new properties
@@ -54,6 +55,7 @@
 #             i.depthFirstSearch(array)
 #         return array
 
+# Number 10
 # def minimumWaitingTime(queries):
 # 	queries.sort()
 # 	sums = 0
@@ -63,23 +65,46 @@
 
 # print(minimumWaitingTime([3, 2, 1, 2, 6]))
 
-def classPhotos(redShirtHeights, blueShirtHeights):
-    pos= 0
-    redShirtHeights.sort(reverse=True)
-    blueShirtHeights.sort(reverse=True)
-    if (redShirtHeights[0] > blueShirtHeights[0]):
-        backrow= redShirtHeights
-        frontrow= blueShirtHeights
-    else:
-        backrow= blueShirtHeights
-        frontrow= redShirtHeights
-    while (pos < len(backrow)):
-        if (backrow[pos] > frontrow[pos]):
-            pos+= 1
-        else:
-            return False
-    return True
+# Number 11
+# def classPhotos(redShirtHeights, blueShirtHeights):
+#     pos= 0
+#     redShirtHeights.sort(reverse=True)
+#     blueShirtHeights.sort(reverse=True)
+#     if (redShirtHeights[0] > blueShirtHeights[0]):
+#         backrow= redShirtHeights
+#         frontrow= blueShirtHeights
+#     else:
+#         backrow= blueShirtHeights
+#         frontrow= redShirtHeights
+#     while (pos < len(backrow)):
+#         if (backrow[pos] > frontrow[pos]):
+#             pos+= 1
+#         else:
+#             return False
+#     return True
 
-# print(classPhotos([6, 9, 2, 4, 5], [5, 8, 1, 3, 4]))
-print(classPhotos([6], [6]))
-print(classPhotos([1, 1, 1, 1, 1, 1, 1, 1], [5, 6, 7, 2, 3, 1, 2, 3]))
+# # print(classPhotos([6, 9, 2, 4, 5], [5, 8, 1, 3, 4]))
+# print(classPhotos([6], [6]))
+# print(classPhotos([1, 1, 1, 1, 1, 1, 1, 1], [5, 6, 7, 2, 3, 1, 2, 3]))
+
+# Number 12
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+    sums= 0
+    if fastest:
+        for i in range(len(redShirtSpeeds)):
+            rider1= redShirtSpeeds[i]
+            rider2= blueShirtSpeeds[len(blueShirtSpeeds) - 1 - i]
+            sums+= max(rider1, rider2)
+    if not fastest:
+        for i in range(len(redShirtSpeeds)):
+            rider1= redShirtSpeeds[i]
+            rider2= blueShirtSpeeds[i]
+            sums+= max(rider1, rider2)
+    return sums
+
+print(tandemBicycle([1, 7, 8], [2, 5, 6], True))
+print(tandemBicycle([1, 7, 8], [2, 5, 6], False))
+
+# Number 13
