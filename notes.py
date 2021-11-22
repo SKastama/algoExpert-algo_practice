c03713ff885e1da9b7318aac20b8d4cc18ab9044
Sarah Kastama <sarahkastama@gmail.com>
2021-11-22 15:58:43 -0800

    notes: ids

--- notes.py
+++ notes.py
@@ -30,20 +30,28 @@ class Book():
 
 book1 = Book("Title1", 72)
 book2 = Book("Title2", 172)
-
 Book.favorites.append(book1)
 Book.favorites.append(book2)
-
 # Prints favorite books
 # for b in Book.favorites:
 #     print(b)
 
 book3 = Book("Title34", 72)
 book4 = Book("Title34", 72)
-
 # Sees if books are the same book
 # print(book3 == book4)
 
 # hashes shouldnt change, two itdentical objects hashes should be the same, 
 # different objects can have the same hashes and its fine
-print(hash(book3) == hash(book4))
+# print(hash(book3) == hash(book4))
+
+# ids of an object, either line leads to the same output
+# print(id(book1), id(book2))
+# print(book1 is book2)
+# Pass by object reference
+def do_something(book):
+    print(id(book))
+    book = Book("Something new", 72)
+    print(id(book))
+do_something(book1)
+print(book1)
