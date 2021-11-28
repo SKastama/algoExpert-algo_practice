f88a68193880070b72ae10edfbf1cf1d894e1e11
Sarah Kastama <sarahkastama@gmail.com>
2021-11-28 12:13:54 -0800

    Number 5

--- medium(0-55).py
+++ medium(0-55).py
@@ -62,6 +62,7 @@ def moveElementToEnd(array, toMove):
 
 # print(moveElementToEnd([2, 4, 2, 5, 6, 2, 2], 2))
 
+
 # Number 4
 def isMonotonic(array):
     # if len(array) <= 1:
@@ -94,6 +95,52 @@ def isMonotonic(array):
             NonIncreasing= False
     return NonDecreasing or NonIncreasing
 
-print(isMonotonic([-1, -1, -2, -3, -4, -5, -5, -5, -6, -7, -8, -8, -9, -10, -11]))
+# print(isMonotonic([-1, -1, -2, -3, -4, -5, -5, -5, -6, -7, -8, -8, -9, -10, -11]))
+
+
+# Number 5
+def spiralTraverse(array):
+    output= []
+    sR= 0
+    sC= 0
+    eR= len(array) - 1
+    eC= len(array[0]) - 1
+	
+    while sR <= eR and sC <= eC:
+        for col in range(sC, eC + 1):
+            output.append(array[sR][col])
+
+        for row in range(sR + 1, eR + 1):
+            output.append(array[row][eC])
+
+        for col in reversed(range(sC, eC)):
+            if sR == eR:
+                break
+            output.append(array[eR][col])
+
+        for row in reversed(range(sR + 1, eR)):
+            if sC == eC:
+                break
+            output.append(array[row][sC])
+        sR+= 1
+        sC+= 1
+        eR-= 1
+        eC-= 1
+    return output
+
+data= [
+    [1,  2,  3,  4],
+    [10, 11, 12, 5],
+    [9,  8,  7,  6],
+]
+data2= [
+    [1,  2,  3],
+    [12, 13, 4],
+    [11, 14, 5],
+    [10, 15, 6],
+    [9,  8,  7],
+]
+
+print(spiralTraverse(data2))
 
 
